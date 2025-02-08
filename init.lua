c8c13f75225674c7e646d3a962c873cc86321e2f
WrongNicholas <mnwojnar2000@gmail.com>
2025-02-08 17:22:45 -0500

    Add custom homepage plugin

--- init.lua
+++ init.lua
@@ -1,12 +1,2 @@
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argc() == 0 then
-      -- No file or directory passed, open Telescope's file picker
-      require("telescope.builtin").find_files()
-    end
-  end,
-})
-
-
 require("config.options")
 require("config.lazy")
---
+++ lua/plugins/homepage.lua
@@ -0,0 +1,8 @@
+return {
+    "WrongNicholas/homepage",
+    lazy = false,  -- Load immediately
+    priority = 1000,  -- Ensure it loads first
+    config = function()
+        require("homepage").setup()
+    end,
+}
