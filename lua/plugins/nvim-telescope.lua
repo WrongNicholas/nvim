ce18b3e4a7b0ae15075b8fcc3f1bd7659b91eb2b
WrongNicholas <mnwojnar2000@gmail.com>
2025-02-07 20:55:59 -0500

    Add barbar.lua, lualine.lua

--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -1,3 +1,5 @@
+-- ~/.config/nvim/lua/plugins/telescope.lua
+
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' },
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,12 +1,14 @@
+-- ~/.config/nvim/lua/plugins/toggleterm.lua
+
 return {
   {'akinsho/toggleterm.nvim',
   version = "*",
   opts = {
-      size = 20,                      
-      direction = "float",           
-      open_mapping = [[<C-\>]],     
+      size = 20,
+      direction = "float",
+      open_mapping = [[<C-\>]],
       float_opts = {
-        border = "curved",         
+        border = "curved",
       },
     },
 
